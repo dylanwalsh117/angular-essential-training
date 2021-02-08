b8544907d204536dbe74490749d7587e9be44715
dylanwalsh117 <Dylan_Walsh@Dell.com>
2021-02-08 13:41:21 +0000

    Updated Code from Module02_07

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import {BrowserModule} from "@angular/platform-browser";
 import {AppComponent} from './app.component';
+import {MediaItemComponent} from "./media-item.component";
 
 @NgModule(
   {
@@ -8,7 +9,8 @@ import {AppComponent} from './app.component';
       BrowserModule
     ],
     declarations: [
-      AppComponent
+      AppComponent,
+      MediaItemComponent
     ],
     bootstrap: [
       AppComponent
